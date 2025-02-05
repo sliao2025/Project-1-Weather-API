7385f452307fd6393e2be4ff5412721344ced435
Steven Liao <steven_y_liao@brown.edu>
2025-02-04 20:41:52 -0500

    test

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-#main
+cities = []
+
+def get_weather(city:str):
+	#make API call
+	cities.append(city)
+	print(cities)
+
+
+def main():
+	while True:
+		location = input("Enter a city name: ")
+
+		get_weather(location)
+
+
+
+main()
