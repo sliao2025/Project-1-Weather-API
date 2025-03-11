3efae13d53857cc827f2d5b16fa3bdb5251bbd8c
Trent Guidero <trextbone88@gmail.com>
2025-03-11 16:38:54 -0700

    bird

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-cities = []
+import requests
+import json
 
-def get_weather(city:str):
-	#make API call
-	cities.append(city)
-	print(cities)
+def get_weather(lat:str, long:str):
+	points_url = f"https://api.weather.gov/points/{lat},{long}"
 
+	response = requests.get(points_url)
+	print(response.status_code)
+	jsonfile = response.json()
+	# dictionary = json.loads(jsonfile)
+	keys = jsonfile.keys()
+	print(list(keys))
+	print(jsonfile["type"])
 
 def main():
 	while True:
-		location = input("Enter a city name: ")
+		lat = input("Enter latitude: ")
+		long = input("Enter longitude: ")
+		
 
-		get_weather(location)
-		if location == 'quit':
-			print('goodbye')
-			break
+		get_weather(lat, long)
+		print("Tempreture: " + '''tempreture''')
+		print("Conditions: " + '''conditions''')
+		print("more stuff that id find on a weather app")
+		# if location == 'quit':
+		# 	print('goodbye')
+		# 	break
 
 main()
